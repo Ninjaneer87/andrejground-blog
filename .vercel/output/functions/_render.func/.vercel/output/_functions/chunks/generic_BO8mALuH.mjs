c391f4763233cae8c588c2f5a34f123c81e20a81
Ninjaneer87 <komodoutd@gmail.com>
2024-06-22 06:50:59 +0200

    configuration fix attempt #4

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_BO8mALuH.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_BL4g2d2Y.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_BSMbF9kV.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_BrxuX19A.mjs').then(n => n.i);
-
-export { page };
