8fdad950fc011905b5995b25ee0e15b0b99e3d04
Ninjaneer87 <komodoutd@gmail.com>
2024-08-05 09:26:37 +0200

    replace article tooltip with popover

--- src/scripts/articleAddTooltip.tsx
+++ src/scripts/articleAddTooltip.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Tooltip } from '@nextui-org/react';
+import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react';
 import { createRoot } from 'react-dom/client';
 
 document.addEventListener('astro:page-load', () => {
   const tooltipElements = Array.from(document.querySelectorAll('.tooltip'));
 
   tooltipElements.forEach(element => {
-    const tooltipContent = element.getAttribute('data-tooltip');
-    const TooltipTriggerContent = element.innerHTML;
+    const content = element.getAttribute('data-tooltip');
+    const triggerContent = element.innerHTML;
     element.innerHTML = '';
     const root = createRoot(element!);
     const triggerElementTag = element.tagName.toLowerCase();
-    const TooltipTrigger = React.createElement(
+    const Trigger = React.createElement(
       triggerElementTag,
-      { className: 'border-b border-dashed border-b-2' },
-      TooltipTriggerContent,
+      { className: 'border-b border-dashed border-b-2 cursor-pointer' },
+      triggerContent,
     );
     root.render(
-      <Tooltip
-        showArrow
-        color="default"
-        content={
-          <span className="max-w-60 p-2 text-primary">{tooltipContent}</span>
-        }
-      >
-        {TooltipTrigger}
-      </Tooltip>,
+      <Popover placement="bottom" showArrow={true}>
+        <PopoverTrigger>{Trigger}</PopoverTrigger>
+        <PopoverContent>
+          <span className="max-w-60 p-2 text-primary">{content}</span>
+        </PopoverContent>
+      </Popover>,
     );
   });
 });
