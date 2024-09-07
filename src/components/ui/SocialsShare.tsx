e85735bbf8a1beac1ea8e54cfb037feeedd40731
Ninjaneer87 <komodoutd@gmail.com>
2024-09-07 07:29:36 +0200

    Add author and share links to blog post body

--- src/components/ui/SocialsShare.tsx
+++ src/components/ui/SocialsShare.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 function SocialsShare({ url }: Props) {
   return (
-    <div className="flex gap-4 items-center py-4 blur-in">
+    <div className="flex gap-2 items-center blur-in">
       <FacebookShare
         url={url}
         bgColor="rgb(var(--secondary))"
