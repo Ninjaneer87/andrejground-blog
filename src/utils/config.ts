32157e075c16e353e1c7916c58c34c33adf00e56
Ninjaneer87 <komodoutd@gmail.com>
2024-09-14 13:42:49 +0200

    Update site meta description

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = 'AndrejGround';
 export const SITE_DESCRIPTION =
-  'A blog about frontend development and career growth.';
+  'A blog about frontend development and career growth';
 export const SITE_URL = 'https://andrejground.com';
 export const SITE_META_IMAGE = `meta-img.webp`;
 export const TWITTER_DOMAIN = 'andrejground.com';
