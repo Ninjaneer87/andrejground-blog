e6d3a1f73fd71d7f0cd60745f1491105d8d1936d
Ninjaneer87 <komodoutd@gmail.com>
2024-09-15 08:49:57 +0200

    Add copy-heading button in articles

---
+++ src/scripts/articleCopyHeading.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { getAllHeadings } from 'src/utils/common';
+import ClipboardCopy from 'src/components/ui/ClipboardCopy';
+
+document.addEventListener('astro:page-load', () => {
+  const { allHeadingsButH1s } = getAllHeadings();
+
+  allHeadingsButH1s.forEach(h => {
+    const id = h.id;
+    const link = `${window.location.href}#${id}`;
+
+    const copySpan = document.createElement('span');
+    copySpan.classList.add('opacity-40', 'ml-2');
+
+    h.insertAdjacentElement('beforeend', copySpan);
+
+    const root = createRoot(copySpan);
+    root.render(<ClipboardCopy text={link} />);
+  });
+});
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -74,3 +74,39 @@ export function getQueryParams(url: string) {
 
   return result;
 }
+
+export type Heading = {
+  id: string;
+  text: string;
+};
+export type H2sAndH3s = {
+  h2: Heading;
+  h3s: Heading[];
+};
+
+export function getH2sAndH3s() {
+  const h2s = Array.from(document.querySelectorAll('h2'));
+  const allH2sAndH3s = Array.from(document.querySelectorAll('h2, h3'));
+  const sectionsLevelOne = Array.from(
+    document.querySelectorAll('[data-heading-rank="2"]'),
+  );
+  const h2sWithH3s = h2s.reduce((acc, h2, i) => {
+    const h3s = Array.from(sectionsLevelOne[i]?.querySelectorAll('h3') ?? []);
+    acc.push({
+      h2: { id: h2.id, text: h2.textContent! },
+      h3s: h3s.map(h3 => ({ id: h3.id, text: h3.textContent! })),
+    });
+
+    return acc;
+  }, [] as H2sAndH3s[]);
+
+  return { h2sWithH3s, allH2sAndH3s };
+}
+
+export function getAllHeadings() {
+  const allHeadingsButH1s = Array.from(
+    document.querySelectorAll('h2, h3, h4, h5, h6'),
+  );
+
+  return { allHeadingsButH1s };
+}
