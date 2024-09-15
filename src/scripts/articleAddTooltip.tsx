dccadd5e088b84ce41da111cc7ea0e8514858c1a
Ninjaneer87 <komodoutd@gmail.com>
2024-09-15 14:31:47 +0200

    Scope other article scripts to article page only

--- src/scripts/articleAddTooltip.tsx
+++ src/scripts/articleAddTooltip.tsx
@@ -3,26 +3,31 @@ import { Popover, PopoverContent, PopoverTrigger } from '@nextui-org/react';
 import { createRoot } from 'react-dom/client';
 
 document.addEventListener('astro:page-load', () => {
-  const tooltipElements = Array.from(document.querySelectorAll('.tooltip'));
+  if (
+    window.location.href.includes('/articles/') &&
+    !window.location.href.includes('authors')
+  ) {
+    const tooltipElements = Array.from(document.querySelectorAll('.tooltip'));
 
-  tooltipElements.forEach(element => {
-    const content = element.getAttribute('data-tooltip');
-    const triggerContent = element.innerHTML;
-    element.innerHTML = '';
-    const root = createRoot(element!);
-    const triggerElementTag = element.tagName.toLowerCase();
-    const Trigger = React.createElement(
-      triggerElementTag,
-      { className: 'border-b border-dashed border-b-2 cursor-pointer' },
-      triggerContent,
-    );
-    root.render(
-      <Popover placement="bottom">
-        <PopoverTrigger>{Trigger}</PopoverTrigger>
-        <PopoverContent>
-          <span className="max-w-60 p-2 text-primary">{content}</span>
-        </PopoverContent>
-      </Popover>,
-    );
-  });
+    tooltipElements.forEach(element => {
+      const content = element.getAttribute('data-tooltip');
+      const triggerContent = element.innerHTML;
+      element.innerHTML = '';
+      const root = createRoot(element!);
+      const triggerElementTag = element.tagName.toLowerCase();
+      const Trigger = React.createElement(
+        triggerElementTag,
+        { className: 'border-b border-dashed border-b-2 cursor-pointer' },
+        triggerContent,
+      );
+      root.render(
+        <Popover placement="bottom">
+          <PopoverTrigger>{Trigger}</PopoverTrigger>
+          <PopoverContent>
+            <span className="max-w-60 p-2 text-primary">{content}</span>
+          </PopoverContent>
+        </Popover>,
+      );
+    });
+  }
 });
--- src/scripts/articleCopyCode.ts
+++ src/scripts/articleCopyCode.ts
@@ -1,58 +1,63 @@
 document.addEventListener('astro:page-load', () => {
-  let copyLabel = 'Copy';
-  const codeBlocks = Array.from(document.querySelectorAll('.astro-code'));
-
-  codeBlocks.forEach(block => {
-    const wrapper = document.createElement('div');
-    wrapper.style.position = 'relative';
-    wrapper.classList.add('code-wrapper');
-
-    const copyButton = document.createElement('button');
-    copyButton.textContent = copyLabel;
-    copyButton.classList.add('copy-button');
-    copyButton.innerText = copyLabel;
-
-    const codeLanguage = block.getAttribute('data-language');
-    const codeLanguageSpan = document.createElement('span');
-    codeLanguageSpan.textContent = codeLanguage;
-    codeLanguageSpan.classList.add('code-language');
-
-    block.parentNode?.insertBefore(wrapper, block);
-    wrapper.appendChild(block);
-    wrapper.appendChild(copyButton);
-    wrapper.appendChild(codeLanguageSpan);
-
-    copyButton.addEventListener(
-      'click',
-      async () =>
-        await copyCode(
-          wrapper as HTMLElement,
-          block as HTMLElement,
-          copyButton,
-        ),
-    );
-  });
-
-  async function copyCode(
-    wrapper: HTMLElement,
-    block: HTMLElement,
-    button: HTMLButtonElement,
+  if (
+    window.location.href.includes('/articles/') &&
+    !window.location.href.includes('authors')
   ) {
-    let code = block.querySelector('code');
-    let text = code?.innerText;
+    let copyLabel = 'Copy';
+    const codeBlocks = Array.from(document.querySelectorAll('.astro-code'));
 
-    if (!text) return;
+    codeBlocks.forEach(block => {
+      const wrapper = document.createElement('div');
+      wrapper.style.position = 'relative';
+      wrapper.classList.add('code-wrapper');
 
-    await navigator.clipboard.writeText(text);
+      const copyButton = document.createElement('button');
+      copyButton.textContent = copyLabel;
+      copyButton.classList.add('copy-button');
+      copyButton.innerText = copyLabel;
 
-    button.innerText = 'Copied!';
-    wrapper.classList.add('active');
-    block.classList.add('active');
+      const codeLanguage = block.getAttribute('data-language');
+      const codeLanguageSpan = document.createElement('span');
+      codeLanguageSpan.textContent = codeLanguage;
+      codeLanguageSpan.classList.add('code-language');
 
-    setTimeout(() => {
-      button.innerText = copyLabel;
-      wrapper.classList.remove('active');
-      block.classList.remove('active');
-    }, 1000);
+      block.parentNode?.insertBefore(wrapper, block);
+      wrapper.appendChild(block);
+      wrapper.appendChild(copyButton);
+      wrapper.appendChild(codeLanguageSpan);
+
+      copyButton.addEventListener(
+        'click',
+        async () =>
+          await copyCode(
+            wrapper as HTMLElement,
+            block as HTMLElement,
+            copyButton,
+          ),
+      );
+    });
+
+    async function copyCode(
+      wrapper: HTMLElement,
+      block: HTMLElement,
+      button: HTMLButtonElement,
+    ) {
+      let code = block.querySelector('code');
+      let text = code?.innerText;
+
+      if (!text) return;
+
+      await navigator.clipboard.writeText(text);
+
+      button.innerText = 'Copied!';
+      wrapper.classList.add('active');
+      block.classList.add('active');
+
+      setTimeout(() => {
+        button.innerText = copyLabel;
+        wrapper.classList.remove('active');
+        block.classList.remove('active');
+      }, 1000);
+    }
   }
 });
