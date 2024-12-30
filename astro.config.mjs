46913ac8794deadd8c8caed318583a2fc684acc3
Ninjaneer87 <komodoutd@gmail.com>
2024-12-30 08:42:52 +0100

    fix vercel import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, envField, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import sectionize from '@hbsnow/rehype-sectionize';
 import react from '@astrojs/react';
 
