dffd18350edd19edafb2f430fe28ab81ccadefc4
Ninjaneer87 <komodoutd@gmail.com>
2025-01-01 08:46:21 +0100

    replace client SECRETS -> import.meta.env

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
-import SECRETS from 'astro:env/server';
+// import SECRETS from 'astro:env/server';
 
 const firebaseConfig = {
-  apiKey: SECRETS.FIREBASE_API_KEY,
-  authDomain: SECRETS.FIREBASE_AUTH_DOMAIN,
-  projectId: SECRETS.FIREBASE_PROJECT_ID,
-  storageBucket: SECRETS.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: SECRETS.FIREBASE_MESSAGING_SENDER_ID,
-  appId: SECRETS.FIREBASE_APP_ID,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
