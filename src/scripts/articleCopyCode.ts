2988fa0c701d845c481ed69294b3dd063acd8c50
Ninjaneer87 <komodoutd@gmail.com>
2025-01-03 19:00:28 +0100

    unduplicate the logic for copy-code

--- src/scripts/articleCopyCode.ts
+++ src/scripts/articleCopyCode.ts
@@ -3,6 +3,9 @@ document.addEventListener('astro:page-load', () => {
     window.location.href.includes('/articles/') &&
     !window.location.href.includes('authors')
   ) {
+    const wrapperExists = document.querySelector('.code-wrapper');
+    if (wrapperExists) return;
+
     let copyLabel = 'Copy';
     const codeBlocks = Array.from(document.querySelectorAll('.astro-code'));
 
@@ -53,11 +56,11 @@ document.addEventListener('astro:page-load', () => {
       wrapper.classList.add('active');
       block.classList.add('active');
 
-      setTimeout(() => {
-        button.innerText = copyLabel;
-        wrapper.classList.remove('active');
-        block.classList.remove('active');
-      }, 1000);
+      // setTimeout(() => {
+      //   button.innerText = copyLabel;
+      //   wrapper.classList.remove('active');
+      //   block.classList.remove('active');
+      // }, 1000);
     }
   }
 });
